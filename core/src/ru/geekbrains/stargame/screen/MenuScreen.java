f232a8c0cf70cc1b19dee97270161437200e619e
StepyginSergey <stepygin.sergey@gmail.com>
2018-10-26 15:23:57 +0300

    Добавлены классы для  математической обработки и система мировых координат переведена в масштаб 42

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -3,30 +3,28 @@ package ru.geekbrains.stargame.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.base.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
 
-    private SpriteBatch batch;
     private Texture img;
 
     private Vector2 pos;
-    private Vector2 v;
     private Vector2 touch;
-    private Vector2 temp;
+    private Vector2 v;
+    private Vector2 buf;
+
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
         pos = new Vector2(0,0);
-        v = new Vector2(0f,0f);
-        touch = new Vector2(0f,0f);
-        temp = new Vector2(0f,0f);
+        touch = new Vector2();
+        v = new Vector2();
+        buf = new Vector2();
     }
 
     @Override
@@ -34,33 +32,28 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(0.128f, 0.53f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, pos.x, pos.y);
-        batch.end();
-
-        temp.set(touch);
-        if(temp.sub(pos).len() >0.5f){
+        buf.set(touch);
+        if (buf.sub(pos).len() > v.len()) {
             pos.add(v);
-        }else{
+        } else {
             pos.set(touch);
         }
+        batch.begin();
+        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
+        batch.end();
 
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        //System.out.println("vector touch len = " + touch.len());
-
-        v.set(touch.cpy().sub(pos).setLength(0.5f));
-        //System.out.println("vector v len = " + v.len());
-        return super.touchDown(screenX, screenY, pointer, button);
+    public boolean touchDown(Vector2 touch, int pointer) {
+        this.touch = touch;
+        v.set(touch.cpy().sub(pos).scl(0.01f));
+        return false;
     }
 }
